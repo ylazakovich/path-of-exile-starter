e8c8c119f2a1ab974c3edbec98c1247e96333bdb
Yaroslav Lazakovich <ylazakovich@Yaroslavs-MacBook-Pro.local>
2023-12-27 19:01:57 +0100

    Added TODOs;

--- path-of-exile-starter-aggregator/src/main/java/io/automation/Main.java
+++ path-of-exile-starter-aggregator/src/main/java/io/automation/Main.java
@@ -8,7 +8,12 @@ public class Main {
 
   /*
   1) TODO: database after reload data id starts not from 1
-  2) TODO:
+  2) TODO: aggregate gems and output profitable goods
+  3) TODO: optimize build gradle
+  4) TODO: build image of service
+  5) TODO: bash script for full service aggregator
+  6) TODO: Controller investigate how to return correct ResponseEntity from Mono/Flex
+  7) TODO: Start telegram integration
    */
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
