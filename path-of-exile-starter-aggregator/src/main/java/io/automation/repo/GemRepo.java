a0249a8cfc547a8669f392a52128113600dfcad4
Yaroslav Lazakovich <ylazakovich@Yaroslavs-MacBook-Pro.local>
2023-12-27 21:19:37 +0100

    [issue-6] Added truncate table after refreshing data;

--- path-of-exile-starter-aggregator/src/main/java/io/automation/repo/GemRepo.java
+++ path-of-exile-starter-aggregator/src/main/java/io/automation/repo/GemRepo.java
@@ -4,10 +4,18 @@ import java.util.Optional;
 
 import io.automation.entity.GemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface GemRepo extends JpaRepository<GemEntity, Long> {
 
   void deleteGemById(Long id);
 
   Optional<GemEntity> findGemById(Long id);
+
+  @Transactional
+  @Modifying
+  @Query(value = "TRUNCATE TABLE gems", nativeQuery = true)
+  void truncateTable();
 }
