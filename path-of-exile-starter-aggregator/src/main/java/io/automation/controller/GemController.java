9af4cec9a714dba8ef69d30e2f84ff585e121d94
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-01-02 18:04:44 +0100

    [main] added new method GemController;

--- path-of-exile-starter-aggregator/src/main/java/io/automation/controller/GemController.java
+++ path-of-exile-starter-aggregator/src/main/java/io/automation/controller/GemController.java
@@ -6,6 +6,7 @@ import io.automation.dto.GemDTO;
 import io.automation.entity.GemEntity;
 import io.automation.service.GemService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,22 @@ public class GemController {
   public List<GemDTO.Gem> findAll() {
     List<GemEntity> entities = gemService.findAllGems();
     return GemDTO.convertToGem(entities);
+  }
+
+  @GetMapping("/findAll/{value}")
+  // TODO: find the way how to optimize method;
+  public List<GemDTO.Gem> findAllWithProfitMoreThan(@PathVariable("value") long value) {
+    List<GemDTO.Gem> data = GemDTO.convertToGem(gemService.findAllGems());
+    List<GemDTO.Gem> maxLevelGems = data.stream()
+        .filter(gem -> gem.getVariant().equals("20") && !gem.isCorrupted())
+        .toList();
+    List<GemDTO.Gem> maxQualityGems = data.stream()
+        .filter(gem -> gem.getVariant().equals("1/20") && !gem.isCorrupted())
+        .toList();
+    return maxLevelGems.stream()
+        .filter(gem -> maxQualityGems.stream()
+            .anyMatch(quality -> gem.getName().equals(quality.getName())
+                && (quality.getChaosValue() - gem.getChaosValue() >= value)))
+        .toList();
   }
 }
