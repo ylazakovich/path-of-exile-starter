0e4e98548648acd8a6a3a38afcc62b7151c1d0e0
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-01-07 21:24:14 +0100

    [#7] Renaming;

--- path-of-exile-starter-aggregator/src/main/java/io/automation/controller/SkillGemController.java
+++ path-of-exile-starter-aggregator/src/main/java/io/automation/controller/SkillGemController.java
@@ -1,9 +1,6 @@
 package io.automation.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import io.automation.dto.SellSkillGemDTO;
 import io.automation.dto.SkillGemDTO;
@@ -24,44 +21,30 @@ public class SkillGemController {
     this.skillGemService = skillGemService;
   }
 
-  @GetMapping("/findAll")
+  @GetMapping("/all")
   public List<SkillGemDTO> findAll() {
     List<SkillGemEntity> entities = skillGemService.findAllGems();
     return SkillGemDTO.convertToList(entities);
   }
 
-  @GetMapping("/findAll/{value}")
-  public List<SkillGemDTO> findAllWithProfitMoreThan(@PathVariable("value") long value) {
-    List<SkillGemDTO> data = findAll();
-    List<SkillGemDTO> maxLevelSkills =
-        data.stream().filter(skillGemDTO -> skillGemDTO.getVariant().equals("20") && !skillGemDTO.isCorrupted())
-            .toList();
-    List<SkillGemDTO> maxQualitySkills =
-        data.stream().filter(skillGemDTO -> skillGemDTO.getVariant().equals("1/20") && !skillGemDTO.isCorrupted())
-            .toList();
-    return maxLevelSkills.stream()
-        .filter(skillGemDTO -> maxQualitySkills.stream()
-            .anyMatch(quality -> skillGemDTO.getName().equals(quality.getName()) &&
-                (quality.getChaosValue() - skillGemDTO.getChaosValue() >= value)))
-        .toList();
+  @GetMapping("/trade/{value}")
+  public List<SellSkillGemDTO> findAllForTrade(@PathVariable("value") long value) {
+    return analyze().stream().filter(skill -> skill.getProfit() >= value).toList();
   }
 
-  // TODO: Not finished
-  @GetMapping("/sell")
-  public List<SellSkillGemDTO> findAllSkillForSell() {
+  // TODO: move to Service
+  public List<SellSkillGemDTO> analyze() {
     List<SkillGemDTO> data = findAll();
     List<SkillGemDTO> maxQualitySkills = data.stream()
         .filter(skillGemDTO -> skillGemDTO.getVariant().equals("1/20") && !skillGemDTO.isCorrupted())
         .toList();
     List<SkillGemDTO> maxLevelSkills = data.stream()
         .filter(skillGemDTO -> skillGemDTO.getVariant().equals("20") && !skillGemDTO.isCorrupted())
-        .filter(skillGemDTO -> maxQualitySkills.stream()
-            .anyMatch(quality -> quality.getName().equals(skillGemDTO.getName())))
         .toList();
-    return substract(maxQualitySkills, maxLevelSkills);
+    return subtract(maxQualitySkills, maxLevelSkills);
   }
 
-  private static List<SellSkillGemDTO> substract(List<SkillGemDTO> quality, List<SkillGemDTO> level) {
+  private static List<SellSkillGemDTO> subtract(List<SkillGemDTO> quality, List<SkillGemDTO> level) {
     return quality.stream()
         .filter(q -> level.stream().anyMatch(l -> l.getName().equals(q.getName())))
         .map(q -> {
