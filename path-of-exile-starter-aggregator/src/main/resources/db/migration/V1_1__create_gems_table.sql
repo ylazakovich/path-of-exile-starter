88fd0316791db075824c52bbddafc7aa93e80ff1
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-01-08 14:56:37 +0100

    [main] Updated scheme of creation table;

--- path-of-exile-starter-aggregator/src/main/resources/db/migration/V1_1__create_gems_table.sql
+++ path-of-exile-starter-aggregator/src/main/resources/db/migration/V1_1__create_gems_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE gems
 (
-    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255) NOT NULL,
-    variant    VARCHAR(255) NOT NULL,
-    corrupted  BOOLEAN      NOT NULL,
-    gemLevel   INTEGER      NOT NULL,
-    gemQuality INTEGER      NOT NULL,
-    chaosValue DOUBLE       NOT NULL
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    variant     VARCHAR(255) NOT NULL,
+    corrupted   BOOLEAN      NOT NULL,
+    gem_level   INTEGER      NOT NULL,
+    gem_quality INTEGER      NOT NULL,
+    chaos_value DOUBLE       NOT NULL
 );
