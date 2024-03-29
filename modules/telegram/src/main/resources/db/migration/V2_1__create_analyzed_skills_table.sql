c0089538c74b5e55d21c75f1aacbe12ad4fcddfd
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-03-29 13:26:45 +0300

    [main] Refactored migration script;

--- modules/telegram/src/main/resources/db/migration/V2_1__create_analyzed_skills_table.sql
+++ modules/telegram/src/main/resources/db/migration/V2_1__create_analyzed_skills_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE analyzed_skills
 (
-    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name      VARCHAR(255),
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255),
     craft_cost DOUBLE NOT NULL,
-    profit    DOUBLE NOT NULL
+    profit     DOUBLE NOT NULL
 );
