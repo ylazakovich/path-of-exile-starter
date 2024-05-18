d42768a7b47866a5016140b4760a65d49e01ca7a
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-05-18 16:44:14 +0200

    [main] Added Selenide, make a template of test;

---
+++ modules/integration-tests/src/test/java/LoginTest.java
@@ -0,0 +1,23 @@
+import static com.codeborne.selenide.Selenide.$;
+
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+  @AfterMethod
+  public void tearDown() {
+    Selenide.closeWebDriver();
+  }
+
+  @Test
+  public void testLogin() {
+    Selenide.open("https://web.telegram.org/k");
+    SelenideElement byPhoneBtn = $(Selectors.byCssSelector("button[class^='btn-primary']"));
+    byPhoneBtn.click();
+    // TODO: Need to make a controller with assigning code for auth then continue test
+  }
+}
