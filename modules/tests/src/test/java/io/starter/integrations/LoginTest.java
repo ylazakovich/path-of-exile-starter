6151de1aac0776560039d50047a141eb4fef13da
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-05-20 17:21:09 +0200

    [issue#56] back headless and comment test;

--- modules/tests/src/test/java/io/starter/integrations/LoginTest.java
+++ modules/tests/src/test/java/io/starter/integrations/LoginTest.java
@@ -13,15 +13,15 @@ public class LoginTest extends BaseUITest {
   @Test()
   void testLogin() {
     Selenide.open("https://web.telegram.org/k");
-    SelenideElement byPhoneBtn = $(Selectors.byCssSelector("button[class^='btn-primary']"));
-    byPhoneBtn.click();
-    SelenideElement phoneField =
-        $(Selectors.byCssSelector("div[class$=input-field-phone] > div[class=input-field-input]"));
-    phoneField.setValue("");
-    SelenideElement nextBtn = $(Selectors.byXpath("//button[contains(@class, 'btn-primary')]/span[text()='Next']"));
-    nextBtn.click();
-    SelenideElement code = $(Selectors.byCssSelector("input[type=tel]"));
-    code.setValue("");
+//    SelenideElement byPhoneBtn = $(Selectors.byCssSelector("button[class^='btn-primary']"));
+//    byPhoneBtn.click();
+//    SelenideElement phoneField =
+//        $(Selectors.byCssSelector("div[class$=input-field-phone] > div[class=input-field-input]"));
+//    phoneField.setValue("");
+//    SelenideElement nextBtn = $(Selectors.byXpath("//button[contains(@class, 'btn-primary')]/span[text()='Next']"));
+//    nextBtn.click();
+//    SelenideElement code = $(Selectors.byCssSelector("input[type=tel]"));
+//    code.setValue("");
 
     // TODO: Part of the other test
 //    SelenideElement searchInput = $(Selectors.byCssSelector("div[class=input-search]"));
