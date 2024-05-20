ad6fa13faf7ad8a4e5f31667e1e569a0612a8260
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-05-20 22:14:05 +0200

    [issue#56] Backed headless.

--- modules/tests/src/test/java/io/starter/BaseUITest.java
+++ modules/tests/src/test/java/io/starter/BaseUITest.java
@@ -1,8 +1,8 @@
 package io.starter;
 
+import com.codeborne.selenide.Configuration;
 import io.starter.listeners.BrowserListener;
 import io.starter.utils.Browser;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
@@ -12,7 +12,7 @@ public abstract class BaseUITest extends BaseTest {
   @BeforeSuite(alwaysRun = true)
   // TODO: Later can be implemented Listeners over injections
   protected void baseSetup() {
-//    Configuration.headless = true;
+    Configuration.headless = true;
     Browser.setDefaultConfigurations();
   }
 }
