6f013636a5b3a0c9abeacfa03b50b3b1c187069d
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-05-21 10:11:47 +0200

    [issue#56] Remove PageTitle.java

--- modules/tests/src/main/java/io/starter/steps/models/Page.java
+++ modules/tests/src/main/java/io/starter/steps/models/Page.java
@@ -1,42 +1,11 @@
 package io.starter.steps.models;
 
-import io.starter.annotations.PageTitle;
 import io.starter.annotations.PageUrl;
 
 /**
  * Interface for Ui page class.
  */
 public interface Page {
-
-  /**
-   * Get title of page.
-   *
-   * @param args dynamic parts in url
-   * @return title as {@link String}
-   */
-  default String getTitle(Object... args) {
-    return getTitle().formatted(args);
-  }
-
-  /**
-   * Get title of page.
-   *
-   * @return title as {@link String}
-   */
-  default String getTitle() {
-    return getClass().getAnnotation(PageTitle.class).value();
-  }
-
-
-  /**
-   * Get url with dynamic parts.
-   *
-   * @param args dynamic parts in url
-   * @return url with dynamic parts as {@link String}
-   */
-  default String getUrl(Object... args) {
-    return getUrl().formatted(args);
-  }
 
   /**
    * Get url.
