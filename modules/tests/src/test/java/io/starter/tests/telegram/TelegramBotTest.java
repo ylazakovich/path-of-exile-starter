b7ed04fba881f63274ed651fe288081b8c599011
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-06-07 20:38:25 +0200

    [issue-109] database
    - move await method inside step
    - update migration scripts
    - update Entity

--- modules/tests/src/test/java/io/starter/tests/telegram/TelegramBotTest.java
+++ modules/tests/src/test/java/io/starter/tests/telegram/TelegramBotTest.java
@@ -1,10 +1,7 @@
 package io.starter.tests.telegram;
 
-import java.time.Duration;
-
 import io.starter.BaseUITest;
 import io.starter.config.AppConfig;
-import io.starter.utils.Waiter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -13,11 +10,6 @@ public class TelegramBotTest extends BaseUITest {
   // TODO: I think that it should be private method which should run only 1 time
   @BeforeClass
   void session() {
-    // TODO: Can be make a call over Supplier
-    Waiter.awaitCondition(() -> !regressionServiceSteps.getValidationCode().equals("0"),
-        "No verification code. Admin can provide it",
-        Duration.ofSeconds(30),
-        Duration.ofMillis(500));
     String code = regressionServiceSteps.getValidationCode();
     telegramNavigationSteps
         .openLoginPage().loginViaPhone(AppConfig.ACCOUNT_PHONE)
