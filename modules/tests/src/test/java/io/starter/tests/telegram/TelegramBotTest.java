57e83b13e6343559cd3769bf6e36630f5941cf25
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-06-07 20:09:07 +0200

    [issue-109] awaitility
    - Added implementation of Awaitility
    - Added session() method: here should be finished realization with login to test account

--- modules/tests/src/test/java/io/starter/tests/telegram/TelegramBotTest.java
+++ modules/tests/src/test/java/io/starter/tests/telegram/TelegramBotTest.java
@@ -1,18 +1,31 @@
 package io.starter.tests.telegram;
 
+import java.time.Duration;
+
 import io.starter.BaseUITest;
 import io.starter.config.AppConfig;
+import io.starter.utils.Waiter;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TelegramBotTest extends BaseUITest {
 
   // TODO: I think that it should be private method which should run only 1 time
-  @Test(enabled = false)
-  void testOnAssigningVerificationCode() {
+  @BeforeClass
+  void session() {
+    // TODO: Can be make a call over Supplier
+    Waiter.awaitCondition(() -> !regressionServiceSteps.getValidationCode().equals("0"),
+        "No verification code. Admin can provide it",
+        Duration.ofSeconds(30),
+        Duration.ofMillis(500));
     String code = regressionServiceSteps.getValidationCode();
     telegramNavigationSteps
         .openLoginPage().loginViaPhone(AppConfig.ACCOUNT_PHONE)
         .inputVerificationCode(code);
+  }
+
+  @Test(enabled = false)
+  void testOnAssigningVerificationCode() {
   }
 
   @Test(enabled = false)
