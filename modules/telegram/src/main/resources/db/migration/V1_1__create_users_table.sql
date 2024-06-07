70325895844bfbc799de19f4214537798e76102c
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-06-07 16:34:08 +0200

    [issue-109] database migration
    - fix entity
    - update table `users`
    - update migration

--- modules/telegram/src/main/resources/db/migration/V1_1__create_users_table.sql
+++ modules/telegram/src/main/resources/db/migration/V1_1__create_users_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE users
     id              BIGINT PRIMARY KEY AUTO_INCREMENT,
     user_id         BIGINT       NOT NULL,
     username        VARCHAR(255) NOT NULL,
+    first_name       VARCHAR(255) NOT NULL,
+    last_name        VARCHAR(255) NOT NULL,
     last_message_id INT
 );
