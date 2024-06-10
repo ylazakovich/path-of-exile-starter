70160a12c132dd5128124446e572d5876672b767
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-06-10 21:06:08 +0200

    [issue-126] code change with init Telegram over Facade

--- modules/telegram/src/main/java/io/starter/TelegramApplication.java
+++ modules/telegram/src/main/java/io/starter/TelegramApplication.java
@@ -1,7 +1,6 @@
 package io.starter;
 
 import io.starter.telegram.service.DatabaseService;
-import io.starter.telegram.service.WebhookService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,7 +12,6 @@ public class TelegramApplication {
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(TelegramApplication.class, args);
-    context.getBean(WebhookService.class).assignToken();
     context.getBean(DatabaseService.class).loadSkills();
   }
 }
--- modules/telegram/src/main/java/io/starter/telegram/model/telegram/Telegram.java
+++ modules/telegram/src/main/java/io/starter/telegram/model/telegram/Telegram.java
@@ -1,14 +1,18 @@
 package io.starter.telegram.model.telegram;
 
+import java.util.function.Function;
+
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.webhook.starter.SpringTelegramWebhookBot;
 
 @Slf4j
 @ToString
 public class Telegram extends SpringTelegramWebhookBot {
 
-  public Telegram(TelegramFacade facade, String token) {
-    super("bot%s".formatted(token), facade::handleOnUpdate, null, null);
+  public Telegram(String token, Function<Update, BotApiMethod<?>> updateHandler, Runnable setWebhook) {
+    super("bot%s".formatted(token), updateHandler, setWebhook, null);
   }
 }
