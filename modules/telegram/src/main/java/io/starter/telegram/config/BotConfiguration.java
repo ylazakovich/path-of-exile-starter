430addc414be95b5c0479052f60c2238912eeb39
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-07-30 21:44:10 +0200

    clean default values

--- modules/telegram/src/main/java/io/starter/telegram/config/BotConfiguration.java
+++ modules/telegram/src/main/java/io/starter/telegram/config/BotConfiguration.java
@@ -7,10 +7,10 @@ import org.aeonbits.owner.Config;
 public interface BotConfiguration extends Config {
 
   @Key("WEBHOOK")
-  @DefaultValue("https://983b-116-202-83-178.ngrok-free.app")
+  @DefaultValue("TODO")
   String webHook();
 
   @Key("TELEGRAM_BOT_TOKEN")
-  @DefaultValue("6983431014:AAF388cP7zOXc-Z1a3B3oO9qzaIrM5qRzbI")
+  @DefaultValue("TODO")
   String token();
 }
