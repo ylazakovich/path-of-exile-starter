c57e413e3eba3e53f55cccc0260ae561a0f6e77d
Yaraslau Lazakovich <ylazakovich@gmail.com>
2024-08-01 21:25:33 +0200

    fix: checkstyle

--- modules/aggregator/src/main/java/io/starter/entity/SkillEntity.java
+++ modules/aggregator/src/main/java/io/starter/entity/SkillEntity.java
@@ -2,7 +2,6 @@ package io.starter.entity;
 
 import java.io.Serializable;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
