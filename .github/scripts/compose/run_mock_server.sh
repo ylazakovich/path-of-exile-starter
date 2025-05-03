bf7530d3002f8a21e4c4aa532dec972baa8d2cbb
Yaraslau Lazakovich <ylazakovich@gmail.com>
2025-05-03 20:04:50 +0200

    added workflow with mock server

--- .github/scripts/compose/run_mock_server.sh
+++ .github/scripts/compose/run_mock_server.sh
@@ -2,4 +2,26 @@
 
 echo "🚀 Starting MockServer container..."
 docker compose up -d --quiet-pull mock-server
-echo "✅ MockServer container started successfully"
+
+sleep=2
+max_count=30
+count=0
+status=0
+
+while true; do
+  status=$(curl -o /dev/null -s -w "%{http_code}" http://localhost:1080/mockserver/dashboard)
+
+  if [[ "$status" -eq 200 ]]; then
+    echo "✅ MockServer container started successfully"
+    break
+  fi
+
+  echo "⏳ Waiting for MockServer... ${count}s elapsed, status code: $status"
+  sleep "$sleep"
+  count=$((count + sleep))
+
+  if [[ "$count" -gt "$max_count" ]]; then
+    echo -e "\e[1;31m❌ MockServer did not start within ${max_count}s (http://localhost:1080)\e[0m"
+    exit 1
+  fi
+done
